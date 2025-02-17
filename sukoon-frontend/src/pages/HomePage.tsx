d4071385aeb355d4ef647bf0537109835e507c9c
Beder Bourahmah <bederbourahmah@gmail.com>
2025-02-16 17:27:05 -0800

    Added frontend dev environment.

---
+++ sukoon-frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold">Welcome to Sukoon</h1>
+      <p>Your Sharia-compliant vehicle financing platform.</p>
+    </div>
+  );
+}
+
+export default HomePage;
